7360daedcc526e2409c9d81ad2fb48f2318e2243
root <root@isilme.(none)>
2016-09-19 14:09:53 +0530

    Login and logout done

---
+++ pages/login.php
@@ -0,0 +1,168 @@
+<?php
+/*
+UserCake Version: 2.0.2
+http://usercake.com
+*/
+
+require_once("models/config.php");
+if (!securePage($_SERVER['PHP_SELF'])){die();}
+
+//Prevent the user visiting the logged in page if he/she is already logged in
+if(isUserLoggedIn()) { header("Location: index.php"); die(); }
+
+//Forms posted
+if(!empty($_POST))
+{
+	$errors = array();
+	$username = sanitize(trim($_POST["username"]));
+	$password = trim($_POST["password"]);
+	
+	//Perform some validation
+	//Feel free to edit / change as required
+	if($username == "")
+	{
+		$errors[] = lang("ACCOUNT_SPECIFY_USERNAME");
+	}
+	if($password == "")
+	{
+		$errors[] = lang("ACCOUNT_SPECIFY_PASSWORD");
+	}
+
+	if(count($errors) == 0)
+	{
+		//A security note here, never tell the user which credential was incorrect
+		if(!usernameExists($username))
+		{
+			$errors[] = lang("ACCOUNT_USER_OR_PASS_INVALID");
+		}
+		else
+		{
+			$userdetails = fetchUserDetails($username);
+			//See if the user's account is activated
+			if($userdetails["active"]==0)
+			{
+				$errors[] = lang("ACCOUNT_INACTIVE");
+			}
+			else
+			{
+				//Hash the password and use the salt from the database to compare the password.
+				$entered_pass = generateHash($password,$userdetails["password"]);
+				
+				if($entered_pass != $userdetails["password"])
+				{
+					//Again, we know the password is at fault here, but lets not give away the combination incase of someone bruteforcing
+					$errors[] = lang("ACCOUNT_USER_OR_PASS_INVALID");
+				}
+				else
+				{
+					//Passwords match! we're good to go'
+					
+					//Construct a new logged in user object
+					//Transfer some db data to the session object
+					$loggedInUser = new loggedInUser();
+					$loggedInUser->email = $userdetails["email"];
+					$loggedInUser->user_id = $userdetails["id"];
+					$loggedInUser->hash_pw = $userdetails["password"];
+					$loggedInUser->title = $userdetails["title"];
+					$loggedInUser->displayname = $userdetails["display_name"];
+					$loggedInUser->username = $userdetails["user_name"];
+					
+					//Update last sign in
+					$loggedInUser->updateLastSignIn();
+					$_SESSION["userCakeUser"] = $loggedInUser;
+					
+					//Redirect to user account page
+					header("Location: index.php");
+					die();
+				}
+			}
+		}
+	}
+}
+
+
+echo resultBlock($errors,$successes);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Bootstrap Admin Theme</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="login-panel panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Please Sign In</h3>
+                    </div>
+                    <div class="panel-body">
+                        <form name="login" role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                            <fieldset>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="Username" name="username" type="text" autofocus>
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="Password" name="password" type="password" value="">
+                                </div>
+                                <div class="checkbox">
+                                    <label>
+                                        <input name="remember" type="checkbox" value="Remember Me">Remember Me
+                                    </label>
+                                </div>
+                                <!-- Change this to a button or input when using this as a form -->
+                                <input type="submit" value="Login" class="btn btn-lg btn-success btn-block">
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="../vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="../vendor/metisMenu/metisMenu.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../dist/js/sb-admin-2.js"></script>
+
+</body>
+
+</html>
